79c495c470e7279da00f260c8571eb111daff05d
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-23 12:43:13 +0200

    Finished and tested exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    
+
+    return array.filter(item => !args.includes(item))
+
+    
+    /*
+    let removedArray = array;
+    let args = Array.from(arguments);
+
+    console.log(args);
+
+    args.shift();
+
+    console.log(args);
+
+    
+    
+    for(x = 0; x <= args.length; x++){
+
+        let indexDelete = removedArray.indexOf(args[x]);
+
+        if(indexDelete > -1){
+            removedArray.splice(indexDelete, 1);
+            --x
+        }
+    }    
+    
+    return removedArray;
+    */
 };
 
 // Do not edit below this line
