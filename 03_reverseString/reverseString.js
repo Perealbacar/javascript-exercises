6d791e5e6cd19eeb401b5b78ffac7cffce15d696
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-23 10:01:45 +0200

    Finished and tested exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(orderedString) {
 
+    let reversedString = "";
+    let stringCharacters = [];
+
+    for(x = 0; x <  orderedString.length; x++) {
+        stringCharacters[x] = orderedString[x];
+    }
+
+    stringCharacters.reverse();
+
+    reversedString = stringCharacters.join("");
+
+    return reversedString;
 };
 
 // Do not edit below this line
