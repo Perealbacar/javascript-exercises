e3c41442bc2270e17a91a467d010ecbdc21572f7
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-23 13:04:03 +0200

    Finished and tested exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
 
+    let sumTotal = 0;
+    let smallNum = firstNum;
+    let bigNum = lastNum;
+
+    const error = 'ERROR';
+
+    if (firstNum > lastNum) {
+        smallNum = lastNum;
+        bigNum = firstNum;
+    }
+    
+    if (firstNum < 0 || lastNum < 0 || firstNum % 1 !== 0  || firstNum % 1 !== 0 || typeof firstNum !== "number" || typeof lastNum !== "number"){
+
+        return error;       
+        
+    }
+    
+
+    for (let a = smallNum ; a <= bigNum; a++){
+        sumTotal += a;
+    }
+
+    return sumTotal;
 };
 
 // Do not edit below this line
