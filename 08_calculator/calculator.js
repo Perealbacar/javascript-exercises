2d309159de7a3df3bf9f93ec5068f23889be4d4e
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-31 11:49:11 +0200

    ex 08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,52 @@
-const add = function() {
+const add = function(a,b) {return a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {return a-b
 	
 };
 
-const sum = function() {
+const sum = function(obj) {
+  
+  
+  let totalSum = obj.reduce(((sum, current) =>  sum + current), 0)
+
+  return totalSum;
+  //return total = totalSum.reduce(((sum, current) =>  sum + current))
+
+  
+ 
 	
 };
 
-const multiply = function() {
+const multiply = function(obj) {
+
+  return total = obj.reduce(((total, current) => total * current),1)
+
+  
 
 };
 
-const power = function() {
+const power = function(base, power) {
+
+  return base ** power
+
+
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+
+  if (number === 0 || number === 1){
+    return 1
+  }
+
+  
+  for (let i = number - 1; i > 0 ; i--){
+    number *= i;
+  }
+
+  return number;
 	
 };
 
