dff690a16726831fc50194f29562f8370b4ee2ee
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-31 14:08:09 +0200

    added exercises untill 11th

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    
+    let stringArray = Array.from(string.toLowerCase());
+
+    stringArray = stringArray.filter(item => item.toUpperCase() != item.toLowerCase() || item <= 9 && item > 0 || item === 0)
+    
+    let reversedArray = stringArray.toReversed();
+
+    const boolstring = stringArray.reduce((result, item, index) => result && item === reversedArray[index], true)
+    
+    return boolstring;
+
+    let bool = true;
+
+    stringArray.map((item, index) =>{
+
+        if(item !== reversedArray[index]){
+            bool = false
+            return        
+        }
+    } )
+
+    return bool
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+   if( index < 0){
+    return "OOPS"
+   }
+
+    let fibSeq = [0,1];
+
+    for(let i = 2; i <= index ; i++){
+
+        let a = i - 2;
+        let b = i - 1;
+
+        fibSeq.push(fibSeq[a] + fibSeq[b]);
+
+    }
+
+    return fibSeq[index];
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
